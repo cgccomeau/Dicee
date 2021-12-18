8951943b17e618a8eaacd12667b7e68dd258b057
Charlie Comeau <comchara@amazon.com>
2021-12-17 17:34:54 -0800

    initial commit

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -10,11 +10,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // IBOutlets allow me to reference a UI element
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
+    // let - turns variable into constant
+    let diceArray = [#imageLiteral(resourceName: "DiceOne"),#imageLiteral(resourceName: "DiceTwo"),#imageLiteral(resourceName: "DiceThree"),#imageLiteral(resourceName: "DiceFour"),#imageLiteral(resourceName: "DiceFive"),#imageLiteral(resourceName: "DiceSix")]
+    
+    // code block that runs every time view is loaded
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
+    // IBActions allow me to reference a button and provide handler code
+    // code block is triggered when button action occurs
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        diceImageView1.image = diceArray.randomElement() // Array.randomElement() = further optimization for Array[Int.random(in:0...Array.count)]
+        diceImageView2.image = diceArray.randomElement()
+        
+    }
 }
+
+/*
+ IBOutlets - for when we want our code to change our user interface
+ IBActions - for when we want an interaction with our UI to run code
+ */
 
